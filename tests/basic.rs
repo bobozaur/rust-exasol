0b0d550895913e6d0bc7b52792a3c28fa4cbfc68
Bogdan Mircea <bogdan.mircea@aeratechnology.com>
2022-01-13 14:29:50 +0200

    Implemented result set and iterator behaviour for it

--- tests/basic.rs
+++ tests/basic.rs
@@ -1,9 +1,14 @@
 //! Tests main crate functionalities, e.g: connecting, executing basic queries, etc.
 #[cfg(test)]
 mod tests {
-    use rust_exasol::exasol::ExaConnection;
-    use serde_json::json;
+    use std::cell::RefCell;
     use std::env;
+    use std::option::IntoIter;
+    use std::rc::Rc;
+
+    use serde_json::json;
+
+    use exasol::exasol::{Connection, QueryResult, ResultSet, Row};
 
     #[test]
     fn it_works() {
@@ -12,19 +17,25 @@ mod tests {
         let user = std::env::var("EXA_USER").unwrap();
         let password = std::env::var("EXA_PASSWORD").unwrap();
 
-        let mut exa_con = ExaConnection::new(&dsn, &schema, &user, &password).unwrap();
+        let mut exa_con = Connection::new(&dsn, &schema, &user, &password).unwrap();
         println!("Connected!");
 
         let result = exa_con.execute("SELECT 1").unwrap();
-        println!("{:?}", result);
-        let result = exa_con.execute("SELECT '1' UNION ALL SELECT '2'").unwrap();
-        println!("{:?}", result);
-        let result = exa_con.execute("SELECT * FROM EXA_ALL_TABLES LIMIT 2000;").unwrap();
-        println!("{:?}", result);
+        // println!("{:?}", result);
+        let result = exa_con.execute("SELECT '1', '2', '3' UNION ALL SELECT '4', '5', '6'").unwrap();
+        // println!("{:?}", result);
+        let result = exa_con.execute("SELECT * FROM DIM_SIMPLE_DATE LIMIT 2000;").unwrap();
+        // println!("{:?}", result);
+        if let QueryResult::ResultSet(mut r) = result {
+            println!("{:?}", r.next());
+        }
         let result = exa_con.execute("DELETE * FROM DIM_SIMPLE_DATE WHERE 1=2").unwrap();
-        println!("{:?}", result);
+        // println!("{:?}", result);
         let results = exa_con.execute_batch(vec!("SELECT 3", "SELECT 4"));
-        println!("{:?}", results);
+        // println!("{:?}", results);
+
+        let results = exa_con.execute_batch(vec!("SELECT 3", "DELETE * FROM DIM_SIMPLE_DATE WHERE 1=2"));
+        // println!("{:?}", results);
 
         exa_con.set_attributes(json!({"autocommit": false})).unwrap();
     }
