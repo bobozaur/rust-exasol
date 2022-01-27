bea21db295068ec6c51c6f9a96ece85013e6df3b
Bogdan Mircea <bogdan.mircea@aeratechnology.com>
2022-01-27 11:48:50 +0200

    Fixed parameter traits implementations

--- tests/basic.rs
+++ tests/basic.rs
@@ -2,6 +2,7 @@
 #[cfg(test)]
 #[allow(unused)]
 mod tests {
+    use std::collections::HashMap;
     use std::env;
 
     use serde_json::json;
@@ -23,16 +24,17 @@ mod tests {
         // println!("{:?}", result);
         let result = exa_con.execute("SELECT '1', '2', '3' UNION ALL SELECT '4', '5', '6'").unwrap();
         // println!("{:?}", result);
-        let result = exa_con.execute("SELECT * FROM DIM_SIMPLE_DATE LIMIT 100;").unwrap();
-        // println!("{:?}", result);
-        if let QueryResult::ResultSet(r) = result {
-            let x = r.take(5000).collect::<Result<Vec<Row>>>();
-            if let Ok(v) = x {
-                for row in v.iter() {
-                    // println!("{:?}", row);
-                }
-            }
-        }
+        // let result = exa_con.execute("SELECT * FROM DIM_SIMPLE_DATE WHERE CALENDARYEAR = 2022;").unwrap();
+        // // println!("{:?}", result);
+        // if let QueryResult::ResultSet(r) = result {
+        //     let x = r.take(5000).collect::<Result<Vec<Row>>>();
+        //     if let Ok(v) = x {
+        //         for row in v.iter() {
+        //             println!("{:?}", row);
+        //         }
+        //     }
+        // }
+
         let result = exa_con.execute("DELETE * FROM DIM_SIMPLE_DATE WHERE 1=2").unwrap();
         // println!("{:?}", result);
         let results = exa_con.execute_batch(vec!("SELECT 3", "SELECT 4"));
