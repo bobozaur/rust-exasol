47a7b27aad9b2140bd5d45d581767c8a24613be6
bobozaur <mirceapetrebogdan@gmail.com>
2022-01-28 14:04:56 +0200

    Added doc index example

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,22 @@
 //!
 //! Database connector for Exasol implemented using the Websocket protocol.
 //! Messages are sent and received in the JSON format.
+//!
+//! ```
+//! use exasol::error::Result;
+//! use exasol::{connect, bind};
+//! use std::env;
+//!
+//! let dsn = env::var("EXA_DSN").unwrap();
+//! let schema = env::var("EXA_SCHEMA").unwrap();
+//! let user = env::var("EXA_USER").unwrap();
+//! let password = env::var("EXA_PASSWORD").unwrap();
+//!
+//! let mut exa_con = connect(&dsn, &schema, &user, &password).unwrap();
+//!
+//! let result = exa_con.execute("SELECT 1").unwrap();
+//!
+//! let result = exa_con.execute("SELECT '1', '2', '3' UNION ALL SELECT '4', '5', '6'").unwrap();
 
 pub mod con_opts;
 pub mod connection;
