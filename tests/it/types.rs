6403550052d86821e6da5765f4a7f129032064a4
Bogdan Mircea <mirceapetrebogdan@gmail.com>
2023-08-13 20:55:25 +0300

    Added empty array check

--- tests/it/types.rs
+++ tests/it/types.rs
@@ -15,6 +15,7 @@ test_type_valid!(bool::"BOOLEAN"::(false, true));
 test_type_valid!(bool_option<Option<bool>>::"BOOLEAN"::("NULL" => None::<bool>, "true" => Some(true)));
 test_type_array!(bool_array<bool>::"BOOLEAN"::(vec![true, false], Vec::<bool>::new(), Some(vec![true, false]), [false; 4], &[false; 4], vec![true, false].into_boxed_slice(), ExaIter::from(HashSet::from([true, false, true]).iter())));
 test_type_array!(bool_array_option<Option<bool>>::"BOOLEAN"::(vec![Some(true), Some(false), None]));
+test_type_array!(bool_empty_array<Option<bool>>::"BOOLEAN"::(Vec::<bool>::new()));
 
 // Unsigned integers
 test_type_valid!(u8::"DECIMAL(3, 0)"::(u8::MIN, u8::MAX));
